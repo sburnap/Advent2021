882b5ea23d2f67300874dcc8c97cea9c7a8d51bb
Steve Burnap <steve@burnap.net>
2021-12-09 12:44:42 -0800

    Clean up

--- Day9/Day9.py
+++ Day9/Day9.py
@@ -1,5 +1,7 @@
+from os import walk
 from typing import List, Tuple, Generator
 from functools import reduce
+from itertools import product
 import operator
 
 testinput = [l.strip() for l in open("Day9/Day9TestInput.txt")]
@@ -11,8 +13,8 @@ def get_height(floormap: List[str], x: int, y: int) -> int:
     return int(floormap[y][x])
 
 
-def risk(height: int) -> int:
-    return height + 1
+def risk(floormap: List[str], x: int, y: int) -> int:
+    return get_height(floormap, x, y) + 1
 
 
 def adjacent(x: int, y: int) -> Generator[Tuple[int, int], None, None]:
@@ -29,40 +31,45 @@ def is_lowpoint(floormap: List[str], x: int, y: int) -> bool:
 
 
 def get_lowpoints(floormap: List[str]):
-    lowpoints = []
-    for y in range(len(floormap)):
-        for x in range(len(floormap[0])):
-            if is_lowpoint(floormap, x, y):
-                lowpoints.append((x, y))
-    return lowpoints
+
+    return [
+        (x, y)
+        for x, y in product(range(len(floormap[0])), range(len(floormap)))
+        if is_lowpoint(floormap, x, y)
+    ]
+
+
+def parse_floormap(filename: str) -> List[str]:
+    return [l.strip() for l in open(filename)]
 
 
 def part_one(filename: str) -> int:
     floormap = [l.strip() for l in open(filename)]
 
-    return sum([risk(get_height(floormap, x, y)) for x, y in get_lowpoints(floormap)])
+    return sum([risk(floormap, x, y) for x, y in get_lowpoints(floormap)])
+
+
+def basin_size(floormap: List[str], lowpoint: Tuple[int]):
+    basin = [lowpoint]
+    i = 0
+    while i < len(basin):
+        for xa, ya in adjacent(*basin[i]):
+            if get_height(floormap, xa, ya) < 9 and (xa, ya) not in basin:
+                basin.append((xa, ya))
+        i += 1
+
+    return len(basin)
 
 
 def part_two(filename: str) -> int:
     floormap = [l.strip() for l in open(filename)]
 
-    lowpoints = get_lowpoints(floormap)
-    sizes = []
-    for lowpoint in lowpoints:
-        basin = [lowpoint]
-        sz = 0
-        i = 0
-        while i < len(basin):
-            x, y = basin[i]
-            sz += 1
-            for xa, ya in adjacent(x, y):
-                if get_height(floormap, xa, ya) < 9 and (xa, ya) not in basin:
-                    basin.append((xa, ya))
-            i += 1
-
-        sizes.append(sz)
-
-    return reduce(operator.mul, sorted(sizes)[-3:])
+    return reduce(
+        operator.mul,
+        sorted(
+            [basin_size(floormap, lowpoint) for lowpoint in get_lowpoints(floormap)]
+        )[-3:],
+    )
 
 
 if __name__ == "__main__":
