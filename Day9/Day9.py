e2f2164fe61650fc8d9eaa9ee9904f2e7a9830b5
Steve Burnap <steve@burnap.net>
2021-12-08 22:15:33 -0800

    mypy

--- Day9/Day9.py
+++ Day9/Day9.py
@@ -1,11 +1,11 @@
-from typing import List
+from typing import List, Tuple, Generator
 from functools import reduce
 import operator
 
 testinput = [l.strip() for l in open("Day9/Day9TestInput.txt")]
 
 
-def get_height(floormap: List[List[str]], x: int, y: int) -> int:
+def get_height(floormap: List[str], x: int, y: int) -> int:
     if x < 0 or y < 0 or x >= len(floormap[0]) or y >= len(floormap):
         return 10
     return int(floormap[y][x])
@@ -15,18 +15,20 @@ def risk(height: int) -> int:
     return height + 1
 
 
-def is_lowpoint(floormap: List[List[str]], x: int, y: int) -> bool:
+def adjacent(x: int, y: int) -> Generator[Tuple[int, int], None, None]:
+    yield (x - 1, y)
+    yield (x + 1, y)
+    yield (x, y - 1)
+    yield (x, y + 1)
+
+
+def is_lowpoint(floormap: List[str], x: int, y: int) -> bool:
     return get_height(floormap, x, y) < min(
-        [
-            get_height(floormap, x - 1, y),
-            get_height(floormap, x + 1, y),
-            get_height(floormap, x, y - 1),
-            get_height(floormap, x, y + 1),
-        ]
+        [get_height(floormap, xa, ya) for xa, ya in adjacent(x, y)]
     )
 
 
-def get_lowpoints(floormap: List[List[str]]):
+def get_lowpoints(floormap: List[str]):
     lowpoints = []
     for y in range(len(floormap)):
         for x in range(len(floormap[0])):
@@ -48,23 +50,16 @@ def part_two(filename: str) -> int:
     sizes = []
     for lowpoint in lowpoints:
         basin = [lowpoint]
-        seen = [lowpoint]
         sz = 0
-        while basin:
-            x, y = basin.pop()
+        i = 0
+        while i < len(basin):
+            x, y = basin[i]
             sz += 1
-            if get_height(floormap, x - 1, y) < 9 and (x - 1, y) not in seen:
-                seen.append((x - 1, y))
-                basin.append((x - 1, y))
-            if get_height(floormap, x + 1, y) < 9 and (x + 1, y) not in seen:
-                seen.append((x + 1, y))
-                basin.append((x + 1, y))
-            if get_height(floormap, x, y - 1) < 9 and (x, y - 1) not in seen:
-                seen.append((x, y - 1))
-                basin.append((x, y - 1))
-            if get_height(floormap, x, y + 1) < 9 and (x, y + 1) not in seen:
-                seen.append((x, y + 1))
-                basin.append((x, y + 1))
+            for xa, ya in adjacent(x, y):
+                if get_height(floormap, xa, ya) < 9 and (xa, ya) not in basin:
+                    basin.append((xa, ya))
+            i += 1
+
         sizes.append(sz)
 
     return reduce(operator.mul, sorted(sizes)[-3:])
