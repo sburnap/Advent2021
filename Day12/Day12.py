4f994317af8816a7acd7f36935af6954e64d86c0
Steve Burnap <steve@burnap.net>
2021-12-12 13:32:00 -0800

    Day12 refactored

--- Day12/Day12.py
+++ Day12/Day12.py
@@ -1,6 +1,6 @@
 from typing import Generator, List, Dict
 import re
-from collections import defaultdict
+from collections import defaultdict, Counter
 
 re_line = re.compile(r"^([a-zA-Z]*)-([a-zA-Z]*)$")
 
@@ -18,24 +18,42 @@ def read_input(filename: str) -> Dict[str, List[str]]:
     return out
 
 
+def eval_good_one(path: List[str]) -> bool:
+    return path[-1].isupper() or path[-1] not in path[:-1]
+
+
+def eval_good_two(path: List[str]) -> bool:
+    if path[-1].isupper():
+        return True
+
+    lowers = tuple(node for node in path if node.islower())
+    return len(set(lowers)) + 1 >= len(lowers)
+
+
 def get_paths(
-    graph: Dict[str, List[str]], start="start"
+    graph: Dict[str, List[str]],
+    eval_good,
 ) -> Generator[List[str], None, None]:
 
-    paths = [[start]]
+    paths = [["start"]]
 
     while len(paths) > 0:
         path = paths.pop()
         for next in graph[path[-1]]:
-            newpath = path + [next]
-            if next == "end":
-                yield newpath
-            elif next.isupper() or next not in path:
-                paths.append(newpath)
+            if next != "start":
+                newpath = path + [next]
+                if next == "end":
+                    yield newpath
+                elif eval_good(newpath):
+                    paths.append(newpath)
 
 
 def part_one(filename: str) -> int:
-    return len([path for path in get_paths(read_input(filename))])
+    return len([path for path in get_paths(read_input(filename), eval_good_one)])
+
+
+def part_two(filename: str) -> int:
+    return len([path for path in get_paths(read_input(filename), eval_good_two)])
 
 
 if __name__ == "__main__":
@@ -44,6 +62,7 @@ if __name__ == "__main__":
     print(f"Testinput2 value is {part_one('Day12/Day12TestInput2.txt')}")
     print(f"Realinput value is {part_one('Day12/Day12Input.txt')}")
 
-    # print("B:")
-    # print(f"Testinput value is {part_two('Day12/Day12TestInput.txt')}")
-    # print(f"Realinput value is {part_two('Day12/Day12Input.txt')}")
+    print("B:")
+    print(f"Testinput1 value is {part_two('Day12/Day12TestInput1.txt')}")
+    print(f"Testinput2 value is {part_two('Day12/Day12TestInput2.txt')}")
+    print(f"Realinput value is {part_two('Day12/Day12Input.txt')}")
