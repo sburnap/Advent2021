094f92fe1e0b44452d02ed79bd57891d892b3bab
Steve Burnap <steve@burnap.net>
2021-12-12 15:12:38 -0800

    Check set on the fly

--- Day12/Day12.py
+++ Day12/Day12.py
@@ -12,8 +12,10 @@ def read_input(filename: str) -> Dict[str, List[str]]:
     out = defaultdict(list)
     for line in open(filename):
         if m := re_line.match(line):
-            out[m.group(1)].append(m.group(2))
-            out[m.group(2)].append(m.group(1))
+            if m.group(2) != "start":
+                out[m.group(1)].append(m.group(2))
+            if m.group(1) != "start":
+                out[m.group(2)].append(m.group(1))
         else:
             raise Exception(f"you done screwed up with {line}")
 
@@ -26,36 +28,34 @@ def eval_good_one(path: Tuple[str], next: str) -> bool:
 
 @functools.lru_cache
 def check_lowers(path: Tuple[str]) -> bool:
-    lowers = tuple(node for node in path if node.islower())
-    return len(set(lowers)) == len(lowers)
+    seen = set()
+    for node in path:
+        if node.islower():
+            if node in seen:
+                return False
+            seen.add(node)
+    return True
 
 
 def eval_good_two(path: Tuple[str], next) -> bool:
 
-    if next not in path:
-        return True
-
-    if next.isupper():
-        return True
-
-    return check_lowers(path)
+    return next.isupper() or next not in path or check_lowers(path)
 
 
 def get_paths(
     graph: Dict[str, List[str]],
     eval_good,
-) -> Generator[Tuple[str], None, None]:
+) -> Generator[Tuple[str, ...], None, None]:
 
-    paths = [("start",)]
+    paths: List[Tuple[str, ...]] = [("start",)]
 
     while len(paths) > 0:
         path = paths.pop()
         for next in graph[path[-1]]:
-            if next != "start":
-                if next == "end":
-                    yield path + (next,)
-                elif eval_good(path, next):
-                    paths.append(path + (next,))
+            if next == "end":
+                yield path
+            elif eval_good(path, next):
+                paths.append(path + (next,))
 
 
 def part_one(filename: str) -> int:
