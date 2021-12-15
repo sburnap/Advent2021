ad2a061c78833657bc4a88682cfb1ccfd32258de
Steve Burnap <steve@burnap.net>
2021-12-15 08:26:26 -0800

    Mypy passes

--- Day15/Day15.py
+++ Day15/Day15.py
@@ -1,4 +1,4 @@
-from typing import Tuple, List, Generator
+from typing import Tuple, List, Generator, Union
 from itertools import product
 from copy import deepcopy
 from heapq import heappush, heappop, heapify
@@ -11,10 +11,10 @@ def get_map(filename: str) -> List[List[int]]:
 def get_bigmap(filename: str) -> List[List[int]]:
     lines = [line.strip() for line in open(filename)]
 
-    bigmap = []
+    bigmap: List[List[int]] = []
     for ty in range(5):
         for y in range(len(lines)):
-            bigmap.append([None] * len(lines[y] * 5))
+            bigmap.append([0] * len(lines[y] * 5))
             for tx in range(5):
                 for x in range(len(lines[y])):
                     bigmap[ty * len(lines) + y][tx * len(lines[0]) + x] = (
@@ -40,27 +40,47 @@ def adjacent(
         yield (x, y + 1)
 
 
-def dijkstra(floormap: List[List[str]]) -> int:
+# Python's heap is ugly
+class PriorityQueue:
+    def __init__(self):
+        self.__data = []
+        heapify(self.__data)
 
-    distances = deepcopy(floormap)
-    for y in range(len(distances)):
-        for x in range(len(distances[0])):
-            distances[y][x] = None
+    def __bool__(self):
+        return True if self.__data else False
+
+    def pop(self):
+        return heappop(self.__data)
+
+    def push(self, val):
+        return heappush(self.__data, val)
+
+
+def its_an_int(x: Union[int, None]) -> int:
+    return x if x else 0
+
+
+def dijkstra(floormap: List[List[int]]) -> int:
+
+    distances: List[List[Union[int, None]]] = [
+        [None for _ in range(len(floormap[0]))] for _ in range(len(floormap))
+    ]
 
     distances[0][0] = floormap[0][0]
-    stack = []
-    heapify(stack)
-    heappush(stack, (distances[0][0], 0, 0))
-    while True:
-        risk, x1, y1 = heappop(stack)
+    queue = PriorityQueue()
+    queue.push((distances[0][0], 0, 0))
+    while queue:
+        _, x1, y1 = queue.pop()
 
         for x, y in adjacent(x1, y1, len(distances[0]) - 1, len(distances) - 1):
             if not distances[y][x]:
                 distances[y][x] = floormap[y][x] + distances[y1][x1]
-                heappush(stack, (distances[y][x], x, y))
+                queue.push((distances[y][x], x, y))
 
         if distances[-1][-1]:
-            return distances[-1][-1] - distances[0][0]
+            return its_an_int(distances[-1][-1]) - its_an_int(distances[0][0])
+
+    raise Exception("Should never happen")
 
 
 def day_one(filename: str) -> int:
