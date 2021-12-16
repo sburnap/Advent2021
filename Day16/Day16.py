ea3cefae7cc69afa73d19b5721fc49c5935abd4a
Steve Burnap <steve@burnap.net>
2021-12-16 11:59:21 -0800

    Day16 done

--- Day16/Day16.py
+++ Day16/Day16.py
@@ -1,6 +1,8 @@
 from typing import Tuple, Dict, Any, List
+import operator
+from functools import reduce
 
-testinput = [
+testinput1 = [
     "D2FE28",
     "38006F45291200",
     "EE00D40C823060",
@@ -8,6 +10,17 @@ testinput = [
     "620080001611562C8802118E34",
     "C0015000016115A2E0802F182340",
     "A0016C880162017C3686B18A3D4780",
+]
+
+testinput2 = [
+    "C200B40A82",
+    "04005AC33890",
+    "880086C3E88112",
+    "CE00C43D881120",
+    "D8005AC2A8F0",
+    "F600BC2D8F",
+    "9C005AC2F8F0",
+    "9C0141080250320F1802104A08",
 ]
 
 
@@ -42,11 +55,13 @@ def parse_number(binary: str) -> Tuple[int, str]:
 
 
 def parse_component(binary: str) -> Tuple[Dict[str, Any], str]:
+    opcodes = ["+", "*", "min", "max", "LIT", ">", "<", "="]
     results = {}
 
     results["version"], binary = parse_version(binary)
-    results["typeid"], binary = parse_version(binary)
-    if results["typeid"] == 4:
+    typeid, binary = parse_version(binary)
+    results["opcode"] = opcodes[typeid]
+    if results["opcode"] == "LIT":
         results["literal"], binary = parse_literal(binary)
     else:
         results["mode"], binary = parse_mode(binary)
@@ -99,19 +114,44 @@ def print_packet(results, tabs=0):
     literal = results["literal"] if "literal" in results else "*"
     if "literal" in results:
         print(
-            f"{'    '*tabs} {results['version']} - {results['typeid']} {results['literal']}"
+            f"{'    '*tabs} {results['version']} : {results['opcode']} {results['literal']}"
         )
     elif "subpacketLength" in results:
         print(
-            f"{'    '*tabs} {results['version']} - {results['typeid']} LEN {results['subpacketLength']}"
+            f"{'    '*tabs} {results['version']} : [{results['subpacketLength']}] {results['opcode']}"
         )
     elif "subpacketNumber" in results:
         print(
-            f"{'    '*tabs} {results['version']} - {results['typeid']} NUM {results['subpacketNumber']}"
+            f"{'    '*tabs} {results['version']} : #{results['subpacketNumber']} {results['opcode']}"
         )
     if "packets" in results:
         for packet in results["packets"]:
             print_packet(packet, tabs + 1)
+
+
+def calculate(results: Dict[str, Any]) -> int:
+
+    opcode = results["opcode"]
+
+    if opcode == "LIT":
+        return results["literal"]
+
+    data = [calculate(packet) for packet in results["packets"]]
+
+    if opcode == "+":
+        return sum(data)
+    elif opcode == "*":
+        return reduce(operator.mul, data, 1)
+    elif opcode == "min":
+        return min(data)
+    elif opcode == "max":
+        return max(data)
+    elif opcode == ">":
+        return 1 if data[0] > data[1] else 0
+    elif opcode == "<":
+        return 1 if data[0] < data[1] else 0
+    elif opcode == "=":
+        return 1 if data[0] == data[1] else 0
 
 
 def part_one(inp: str, display: bool) -> int:
@@ -121,15 +161,25 @@ def part_one(inp: str, display: bool) -> int:
     return version_sum(parsed)
 
 
+def part_two(inp: str, display: bool) -> int:
+    parsed, _ = parse_component(bin(int(inp, 16))[2:].zfill(len(inp * 4)))
+    if display:
+        print_packet(parsed)
+    return calculate(parsed)
+
+
 if __name__ == "__main__":
 
     print("A:")
-    for line in testinput:
-        print(f"Testinput value is {part_one(line, True)}")
+    for line in testinput1:
+        print(f"Testinput value is {part_one(line, False)}")
     print(
         f"Realinput value is {part_one(next(line.strip() for line in open('Day16/Day16Input.txt')), False)}"
     )
 
     print("B:")
-    # print(f"Testinput value is {day_two('Day16/Day16TestInput.txt')}")
-    # print(f"Realinput value is {day_two('Day16/Day16Input.txt')}")
+    for line in testinput2:
+        print(f"Testinput value {line} is {part_two(line, False)}")
+    print(
+        f"Realinput value is {part_two(next(line.strip() for line in open('Day16/Day16Input.txt')), False)}"
+    )
